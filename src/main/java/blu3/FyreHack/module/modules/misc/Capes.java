b315dc622aa0f7d5fdb6bf3cb91f5b574951e42c
blu3 <67823680+blu3-65b@users.noreply.github.com>
2020-08-13 11:26:33 -0700

    ihjbu

---
+++ src/main/java/blu3/FyreHack/module/modules/misc/Capes.java
@@ -0,0 +1,10 @@
+package blu3.FyreHack.module.modules.misc;
+
+import blu3.FyreHack.module.Category;
+import blu3.FyreHack.module.Module;
+
+public class Capes extends Module {
+    public Capes() {
+        super("Capes", 0, Category.MISC, "Turns on capes for other fern users", null);
+    }
+}
