a75815dc2ee31a8be88dc73f03c0c1aada50d36c
blu3 <67823680+blu3-65b@users.noreply.github.com>
2020-09-23 16:37:36 -0700

    Update Criticals.java

--- src/main/java/blu3/ruhamaplus/module/modules/combat/Criticals.java
+++ src/main/java/blu3/ruhamaplus/module/modules/combat/Criticals.java
@@ -5,7 +5,7 @@ import blu3.ruhamaplus.module.Module;
 import net.minecraft.network.play.client.CPacketPlayer;
 
 public class Criticals extends Module {
-    public Criticals() { super("Criticals", 0, Category.CHAT, "HE HACER HE NO JUMP BUT CRITICAL", null); }
+    public Criticals() { super("Criticals", 0, Category.COMBAT, "HE HACER HE NO JUMP BUT CRITICAL", null); }
 
     public void performCritical(){
         mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.1f, mc.player.posZ, false));
