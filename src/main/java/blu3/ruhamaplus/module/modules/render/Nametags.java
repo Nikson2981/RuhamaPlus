cfbf6739dd1748f91b1f48bf5f0c90e33c8e63fa
blu3 <67823680+blu3-65b@users.noreply.github.com>
2020-12-16 22:14:26 -0800

    Update Nametags.java

--- src/main/java/blu3/ruhamaplus/module/modules/render/Nametags.java
+++ src/main/java/blu3/ruhamaplus/module/modules/render/Nametags.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Nametags extends Module {
     public Nametags() { super("Nametags", 0, Category.RENDER, "now THAAAATS a lotta players", null); }
 
+    //please for the love of god dont use this
     public void onRender() {
         List<EntityPlayer> players = new ArrayList<>(this.mc.world.playerEntities);
         players.remove(this.mc.player);
